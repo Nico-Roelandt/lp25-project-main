13711aedb3b4d8076d37169cd1f1a99fa1aa2c96
Milenabravo <milena.bravo@utbm.fr>
2022-12-24 12:47:22 +0100

    Configuration et utility

--- configuration.c
+++ configuration.c
@@ -22,6 +22,164 @@
  * @return the pointer to the updated base configuration
  */
 configuration_t *make_configuration(configuration_t *base_configuration, char *argv[], int argc) {
+
+    int opt = 0;
+    while ((opt = getopt(argc, argv, "d:o:t:v:n:f:")) != -1) {
+        switch (opt) {
+            case 'd': //Parametrage data path
+                if (optarg) {
+                    if(directory_exists(optarg)==true){
+                        strcpy(base_configuration->data_path,optarg);
+                    }else{
+                        base_configuration=NULL;
+                    }
+                    break;
+                }
+                if (optind == argc || (optind < argc && argv[optind][0] == '-')) {
+                    base_configuration=NULL;
+                    break;
+                }
+                if (optind < argc && argv[optind][0] != '-') {
+                    if(directory_exists(argv[optind])==true){
+                        strcpy(base_configuration->data_path,argv[optind]);
+                    }else{
+                        base_configuration=NULL;
+                    }
+                    ++optind;
+                    break;
+                }
+                break;
+            case 'o': //Parametrage output file
+                if (optarg) {
+                    if( path_to_file_exists(optarg)==true){
+                        strcpy(base_configuration->output_file,optarg);
+                    }else{
+                        base_configuration=NULL;
+                    }
+                    break;
+                }
+                if (optind == argc || (optind < argc && argv[optind][0] == '-')) {
+                    break;
+                }
+                if (optind < argc && argv[optind][0] != '-') {
+                    if( path_to_file_exists(optarg)==true){
+                        strcpy(base_configuration->output_file,argv[optind]);
+                    }else{
+                        base_configuration=NULL;
+                    }
+                    ++optind;
+                    break;
+                }
+                break;
+            case 't': //Parametrage temporary directory
+                if (optarg) {
+                    if(directory_exists(optarg)==true){
+                        strcpy(base_configuration->temporary_directory,optarg);
+                    }else{ //sinon on cree un repertoire
+                        int etat=create_directory(optarg);
+                        if(etat!=0){
+                            base_configuration=NULL;
+                        }
+                    }
+                    break;
+                }
+                if (optind == argc || (optind < argc && argv[optind][0] == '-')) {
+                    break;
+                }
+                if (optind < argc && argv[optind][0] != '-') {
+                    printf("Option 'b' with optional argument=%s\n", argv[optind]);
+                    if(directory_exists(argv[optind])==true){
+                        strcpy(base_configuration->temporary_directory,argv[optind]);
+                    }else{ //sinon on cree un repertoire
+                        int etat=create_directory(argv[optind]);
+                        if(etat!=0){
+                            base_configuration=NULL;
+                        }
+                    }
+                    ++optind;
+                    break;
+                }
+                break;
+
+            case 'v': //Parametrage verbose
+                if (optarg) {
+                    if(strncmp(optarg,"no",strlen("no"))==0){
+                        base_configuration->is_verbose=false;
+                    }else if(strncmp(optarg,"yes",strlen("yes"))==0){
+                        base_configuration->is_verbose=true;
+                    }else{
+                        base_configuration=NULL;
+                    }
+                    break;
+                }
+                if (optind == argc || (optind < argc && argv[optind][0] == '-')) {
+                    printf("Option 'b' with no argument\n");
+                    break;
+                }
+                if (optind < argc && argv[optind][0] != '-') {
+                    if(strncmp(argv[optind],"no",strlen("no"))==0){
+                        base_configuration->is_verbose=false;
+                    }else if(strncmp(argv[optind],"yes",strlen("yes"))==0){
+                        base_configuration->is_verbose=true;
+                    }else{
+                        base_configuration=NULL;
+                    }
+
+                    ++optind;
+                    break;
+                }
+                break;
+
+            case 'n': //Parametrage cpu core
+                if (optarg) {
+                    base_configuration->cpu_core_multiplier=atoi(optarg);
+                    break;
+                }
+                if (optind == argc || (optind < argc && argv[optind][0] == '-')) {
+                    break;
+                }
+                if (optind < argc && argv[optind][0] != '-') {
+                    base_configuration->cpu_core_multiplier=atoi(optarg);
+                    ++optind;
+                    break;
+                }
+            case 'f': //Parametrage  avec lecture de fichier
+                if (optarg) {
+                    if(path_to_file_exists(optarg)==true){
+                        base_configuration=read_cfg_file(base_configuration,optarg);
+                    }else{
+                        base_configuration=NULL;
+                    }
+                    break;
+                }
+                if (optind == argc || (optind < argc && argv[optind][0] == '-')) {
+                    base_configuration=NULL;
+                    break;
+                }
+                if (optind < argc && argv[optind][0] != '-') {
+                    if(path_to_file_exists(optarg)==true){
+                        base_configuration=read_cfg_file(base_configuration,argv[optind]);
+
+                    }else{
+                        base_configuration=NULL;
+                    }
+                    ++optind;
+                    break;
+                }
+                break;
+            default:
+                base_configuration=NULL;
+                printf("Wrong option or missing argument for option\n");
+        }
+    }
+    if (optind < argc) {
+        printf("Remaining program arguments:");
+        for (int i=optind; i<argc; ++i) {
+            printf(" %s", argv[i]);
+        }
+        printf("\n");
+    }
+
     return base_configuration;
 }
 
@@ -31,6 +189,12 @@ configuration_t *make_configuration(configuration_t *base_configuration, char *a
  * @return a pointer to the first non-space character in str
  */
 char *skip_spaces(char *str) {
+    int i=0;
+    while(str[i]==' '&& str[i]!='\0'){
+        i++;
+    }
+    str=&str[i];
+
     return str;
 }
 
@@ -41,6 +205,36 @@ char *skip_spaces(char *str) {
  * @return a pointer to the first non-space character after the =, NULL if no equal was found
  */
 char *check_equal(char *str) {
+    int i=0;
+
+
+    if(str[0]==' '){ // Pointer to the first non espace
+        str=skip_spaces(str);
+    }
+
+    while(str[i]!='='&& str[i]!='\0'){ //looks for an equal '=' sign
+        i++;
+    }
+
+    if(strlen(str)==i+1){  //no equal was found
+        str=NULL;
+
+    }else{
+        str=&str[i]; // Pointer to the first '=' if an equal was found
+        i=0;
+        if(str[i+1]==' '){ //looks for an optional sequence of spaces after the equal
+            str=&str[i+1];// Pointer to the first ' ' if found after an equal
+            str=skip_spaces(str); // Pointer to the first char autre que " "
+
+        }else{
+            str=&str[i+1];
+        }
+
+
+    }
+
+
+
     return str;
 }
 
@@ -51,6 +245,24 @@ char *check_equal(char *str) {
  * @return a pointer to the character after the end of the extracted word
  */
 char *get_word(char *source, char *target) {
+    int i=0;
+
+    if(source[0]==' '){ //looks for an optional sequence of spaces at the beginning
+        source=skip_spaces(source);
+    }
+    while(source[i]!=' '&&source[i]!='\0'){ //
+        i++;
+    }
+
+    if(source[i-1]=='\n'){  // si le derniere caractere est un saut de page
+        strncpy(target,source,i-1);
+
+    }else{
+        strncpy(target,source,i);
+    }
+
+
+
     return source;
 }
 
@@ -62,6 +274,124 @@ char *get_word(char *source, char *target) {
  * @return a pointer to the base configuration after update, NULL is reading failed.
  */
 configuration_t *read_cfg_file(configuration_t *base_configuration, char *path_to_cfg_file) {
+    int test_next_ligne=0;
+    char string[STR_MAX_LEN];
+    char *source;
+    char extract_string[STR_MAX_LEN];
+    if(path_to_file_exists(path_to_cfg_file)==true) {
+
+        FILE *f = fopen(path_to_cfg_file, "r");
+        while (fgets(string, STR_MAX_LEN, f)) {
+
+
+            if(test_next_ligne==1){  //Saut de ligne pour le parametrage de temporary file
+                test_next_ligne=0;
+                get_word(string,extract_string);
+                if(directory_exists(extract_string)==true){
+                    strcpy(base_configuration->temporary_directory,extract_string);
+                }
+                else{ //sinon on cree un repertoire
+                    int etat=create_directory(extract_string);
+                    if(etat!=0){
+                        base_configuration=NULL;
+                    }
+                }
+            }
+
+
+            if(strncmp(string,"is_verbose",strlen("is_verbose"))==0){   //Parametrage verbose
+
+                if(check_equal(string)!=0){
+
+                    if(strncmp(check_equal(string),"no",strlen("no"))==0){
+                        base_configuration->is_verbose=false;
+                    }else if(strncmp(check_equal(string),"yes",strlen("yes"))==0){
+                        base_configuration->is_verbose=true;
+                    }else{
+                        base_configuration=NULL;
+                    }
+
+                }
+            }
+
+            if(strncmp(string,"data_path",strlen("data_path"))==0){ //Parametrage data path
+                source=check_equal(string);
+                if(source!=0){
+                    get_word(source,extract_string);
+                    if(directory_exists(extract_string)==true){
+                        strcpy(base_configuration->data_path,extract_string);
+                    }else{
+                        base_configuration=NULL;
+                    }
+
+                }else{
+                    base_configuration=NULL;
+                }
+            }
+
+            if(strncmp(string,"temporary_directory",strlen("temporary_directory"))==0){ //Parametrage temporary directory
+                source=check_equal(string);
+                if(source!=0){
+                    if(strcmp(source,"\n")==0){
+                        test_next_ligne=1;
+                    }else{
+                        get_word(source,extract_string);
+                        if(directory_exists(extract_string)==true){
+                            strcpy(base_configuration->temporary_directory,extract_string);
+                        }
+                        else{ //sinon on cree un repertoire
+                            int etat=create_directory(extract_string);
+                            if(etat!=0){
+                                base_configuration=NULL;
+                            }
+                        }
+
+                    }
+
+                }else{
+                    base_configuration=NULL;
+                }
+            }
+
+
+
+
+            if(strncmp(string,"output_file",strlen("output_file"))==0){ //Parametrage output file
+                source=check_equal(string);
+                if(source!=0){
+                    get_word(source,extract_string);
+                    if( path_to_file_exists(extract_string)==true){
+                        strcpy(base_configuration->output_file,extract_string);
+                    }else{
+                        base_configuration=NULL;
+                    }
+
+                }else{
+                    base_configuration=NULL;
+                }
+            }
+            if(strncmp(string,"cpu_core_multiplier",strlen("cpu_core_multiplier"))==0){
+                source=check_equal(string);
+                if(source!=0){
+                    get_word(source,extract_string);
+                    base_configuration->cpu_core_multiplier=atoi(extract_string);
+                }else{
+                    base_configuration=NULL;
+                }
+
+            }
+
+
+            memset(extract_string,'\0',STR_MAX_LEN);
+            memset(string,'\0',STR_MAX_LEN);
+        }
+    }
+    /**
+    else{
+        base_configuration=NULL;
+    }
+*/
+
     return base_configuration;
 }
 
@@ -87,5 +417,12 @@ void display_configuration(configuration_t *configuration) {
  * @return true if configuration is valid, false else
  */
 bool is_configuration_valid(configuration_t *configuration) {
-    return false;
+    if(configuration==NULL){
+        return false;
+    }else{
+        return true;
+    }
 }
+
+
+
--- utility.c
+++ utility.c
@@ -9,6 +9,12 @@
 #include <libgen.h>
 #include <unistd.h>
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<libgen.h>
+#include <sys/stat.h>
+
 #include "global_defs.h"
 
 /*!
@@ -19,6 +25,15 @@
  * @return pointer to full_path if operation succeeded, NULL else
  */
 char *concat_path(char *prefix, char *suffix, char *full_path) {
+    if(prefix!=NULL&&suffix!=NULL){
+        strcpy(full_path,prefix);
+        strcat(full_path,suffix);
+    }
+    else{
+        strcpy(full_path,NULL);
+    }
+
+
     return full_path;
 }
 
@@ -28,6 +43,15 @@ char *concat_path(char *prefix, char *suffix, char *full_path) {
  * @return true if directory exists, false else
  */
 bool directory_exists(char *path) {
+    if (opendir(path)==NULL) // Si une
+    {
+        //printf("%s ",strerror(errno))
+        return false;
+    }
+    else{
+        //printf("exte %s",path);
+        return true;
+    }
     return false;
 }
 
@@ -39,6 +63,16 @@ bool directory_exists(char *path) {
  * @return true if path to file exists, false else
  */
 bool path_to_file_exists(char *path) {
+    FILE *f=fopen(path, "a+");
+    if(f==NULL){
+        //printf("%s ",strerror(errno));
+        return false;
+    }else{
+
+        //printf("chemin trouve");
+        return true;
+
+    }
     return false;
 }
 
@@ -48,6 +82,8 @@ bool path_to_file_exists(char *path) {
  * Use fsync and dirfd
  */
 void sync_temporary_files(char *temp_dir) {
+
+
 }
 
 /*!
@@ -56,6 +92,85 @@ void sync_temporary_files(char *temp_dir) {
  * @param dir a pointer to the already opened directory
  * @return a pointer to the next not . or .. directory, NULL if none remain
  */
-struct dirent *next_dir(struct dirent *entry, DIR *dir) {
+struct dirent *next_dir(struct dirent *entry, DIR *dir) { //PAS ENCORE FINIE
+    int stat_change_directory=-1;
+    int test_first_dir=0;
+    //char path[STR_MAX_LEN];
+    char *path=(char*)malloc(STR_MAX_LEN*sizeof(char));
+
+    if (!dir) {
+        printf("is not a valid path\n");
+
+    } else {
+        while ((entry = readdir(dir)) != NULL&&test_first_dir==0){
+
+            struct stat info;
+            stat(entry->d_name, &info);
+            if (strcmp(entry->d_name, ".") && strcmp(entry->d_name, "..")&&S_ISDIR(info.st_mode)) {
+                printf("%s is a directory\n", entry->d_name);
+                //realpath(entry->d_name, path);
+                puts(path);
+                stat_change_directory=chdir(path);
+                if(stat_change_directory==0){
+                    printf("change of directory successful");
+                }else{
+                    printf("change of directory unsuccessful");
+                }
+                test_first_dir=1;
+            }
+        }
+        closedir(dir);
+        return entry;
+    }
     return NULL;
 }
+
+/*!
+ * @brief Creates a new repertory with th
+ * @param path: chemin absolue contenant repertoire a creer et repertoire parent
+ * @return 1 si le repertoire a ete cree et NULL sinon
+ */
+int create_directory(char*path){
+    int stat_change_directory=-1;
+    int stat_create_directory=-1;
+    int lg_lastdir,lg_path,lg_parentdir;
+
+    char last_dir[STR_MAX_LEN];
+    char parent_dir[STR_MAX_LEN];
+
+    strcpy(last_dir, basename(path));   // On extrait le nom du repertoire a creer
+    lg_lastdir=strlen(last_dir);
+    lg_path=strlen(path);
+    lg_parentdir=lg_path-lg_lastdir;
+    strncpy(parent_dir, path,lg_parentdir-1); // On extrait le chemin des repertoires parents
+
+
+    if(directory_exists(parent_dir)){
+        stat_change_directory=chdir(parent_dir);
+        if(stat_change_directory==0){ //Change of direcctory succesful
+            stat_create_directory=mkdir(last_dir, 0777);
+            if ( stat_create_directory== 0) { //Directory created successfully
+                //printf("Directory created successfully\n");
+            } else {
+                //perror("mkdir");
+                return 0;
+            }
+        }else{
+            return 0;
+        }
+
+    }else{
+        return 0;
+    }
+
+
+    return 1;
+}
+
+
+
+
+
+
+
+
